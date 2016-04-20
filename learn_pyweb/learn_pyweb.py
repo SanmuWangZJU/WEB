d2823704322e64cd7acada86629832a6c3790c50
SanmuWangZJU <sanmuwang.ws@gmail.com>
2016-04-20 16:16:06 +0800

    just add some comments

--- learn_pyweb/learn_pyweb.py
+++ learn_pyweb/learn_pyweb.py
@@ -1,19 +1,22 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import logging
+# 设置日志等级,默认为WARNING.只有指定级别或更高级的才会被追踪记录
+logging.basicConfig(level=logging.INFO)
 import asyncio, os, json, time
 from datetime import datetime
 from aiohttp import web
 
-
+# 对根目录请求的处理函数
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
-
+# 初始化协程
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
+    app = web.Application(loop=loop) # 创建一个循环类型是消息循环的web应用对象
+    app.router.add_route('GET', '/', index) # 为"/"路径的"GET"请求添加处理,处理函数为index
+    # 调用子协程:创建一个TCP服务器,绑定到"127.0.0.1:9000"socket,并返回一个服务器对象
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
+loop.run_until_complete(init(loop))#在消息循环中执行协程
 loop.run_forever()
